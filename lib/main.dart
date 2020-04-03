c2a8aab17a1d50fcded4c69a128ed4a07d518388
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-04-03 18:46:06 -0300

    Criação da AppBar e do TextField + botão do cadastro de tarefas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main(){
+void main() {
   runApp(CreateApp());
 }
 
@@ -28,7 +29,40 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Nova Tarefa",
+                          labelStyle: TextStyle(color: Colors.blueAccent)
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blueAccent,
+                    child: Text("ADD"),
+                    textColor: Colors.white,
+                    onPressed: (){},
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Future<File> _getFile() async {
@@ -51,6 +85,3 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 }
-
-
-
