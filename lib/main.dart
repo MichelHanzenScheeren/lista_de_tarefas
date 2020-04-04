142aaa8c28d6f802439cd700c39c7659491a94a6
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-04-04 14:47:53 -0300

    Adição da lógica básica de inserção dos elementos na ListView.
    Tamém houve a remoção do elemento externo "SingleChildScrollView", que estava causando erros de renderização por causa do ListView.

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _toDoController = TextEditingController();
   List _toDoList = [];
+
+  void _addToDoList() {
+    Map<String, dynamic> newToDo = Map();
+    newToDo["title"] = _toDoController.text;
+    newToDo["finished"] = false;
+    setState(() {
+      _toDoList.add(newToDo);
+      _toDoController.clear();
+    });
+  }
+
+  void _changeState(bool finished, int index) {
+    setState(() {
+      _toDoList[index]["finished"] = finished;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,32 +52,46 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                          labelText: "Nova Tarefa",
-                          labelStyle: TextStyle(color: Colors.blueAccent)
-                      ),
-                    ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
                   ),
-                  RaisedButton(
-                    color: Colors.blueAccent,
-                    child: Text("ADD"),
-                    textColor: Colors.white,
-                    onPressed: (){},
-                  )
-                ],
-              ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: _addToDoList,
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index) {
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index]["title"]),
+                    value: _toDoList[index]["finished"],
+                    secondary: CircleAvatar(
+                        child: Icon(_toDoList[index]["finished"]
+                            ? Icons.check
+                            : Icons.error)),
+                    onChanged: (finished) => _changeState(finished, index)
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
